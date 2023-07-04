75d6e064707a8cc66b3c460155635c644acc0cc8
frog <hello@frogtools.xyz>
2023-07-04 14:33:31 -0400

    update layout.tsx, page.tsx and TextArea.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { TextArea } from "@/components";
+import { TextArea, Button, Icon } from "@/components";
 export default async function Home() {
   return (
     <div className="relative z-10 mb-4 flex w-full flex-col gap-y-4">
-      <p> Hello World </p>
       <TextArea />
+      <Button startIcon={<Icon icon={{ prefix: "fas", iconName: "paper-plane" }} />}>
+        <p>Send</p>
+      </Button>
     </div>
   );
 }
--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -10,16 +10,28 @@ import CodeMirror from "@uiw/react-codemirror";
 const myTheme = createTheme({
   theme: "dark",
   settings: {
-    background: "#ffffff",
-    foreground: "#4D4D4C",
-    caret: "#AEAFAD",
-    selection: "#D6D6D6",
-    selectionMatch: "#D6D6D6",
-    gutterBackground: "#FFFFFF",
-    gutterForeground: "#4D4D4C",
-    gutterBorder: "#dddddd",
-    gutterActiveForeground: "",
-    lineHighlight: "#EFEFEF",
+    /** Editor background. */
+    background: "#111b24", //0
+    /** Default text color. */
+    foreground: "#01ebc7", //1
+    /** Caret color. */
+    caret: "#6BABE3", //2
+    /** Selection background. */
+    selection: "#2F4B63", //1
+    /** Selection match background. */
+    selectionMatch: "#1080E3", //3
+    /** Background of highlighted lines. */
+    // lineHighlight?: string;
+    /** Gutter background. */
+    gutterBackground: "#111b24", //0
+    /** Text color inside gutter. */
+    gutterForeground: "#33bca7", //2
+    /** Text active color inside gutter. */
+    gutterActiveForeground: "#fff",
+    /** Gutter right border color. */
+    gutterBorder: "#5F98C9", //3
+    /** set editor font */
+    // fontFamily?: string;
   },
   styles: [{ tag: t.integer, color: "#692adf" }],
 });
@@ -28,7 +40,7 @@ export const TextArea = () => {
   return (
     <CodeMirror
       placeholder={`frog.azero, 9001`}
-      height="200px"
+      height="260px"
       theme={myTheme}
       extensions={[zebraStripes({ step: 2, lightColor: "#aca2ff33", darkColor: "#aca2ff40" })]}
       onChange={(value, viewUpdate) => {
