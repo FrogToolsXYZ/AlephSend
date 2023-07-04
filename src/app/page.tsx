0299e6aec8d7dcfd56051cd249f3b2b54be4758e
frog <hello@frogtools.xyz>
2023-07-04 14:47:05 -0400

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,14 @@ export default async function Home() {
   return (
     <div className="relative z-10 mb-4 flex w-full flex-col gap-y-4">
       <TextArea />
-      <Button startIcon={<Icon icon={{ prefix: "fas", iconName: "paper-plane" }} />}>
-        <p>Send</p>
-      </Button>
+      <div className="row-auto flex justify-center">
+        <Button startIcon={<Icon icon={{ prefix: "fas", iconName: "cloud-arrow-up" }} />}>
+          <p>Upload CSV</p>
+        </Button>
+        <Button startIcon={<Icon icon={{ prefix: "fas", iconName: "paper-plane" }} />}>
+          <p>Send</p>
+        </Button>
+      </div>
     </div>
   );
 }
